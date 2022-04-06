98018673e9db9989f471d25b81d0a49f2bc3793b
SergiITB <sergi.santos.7e4@itb.cat>
2022-04-06 20:50:28 +0200

    .

---
+++ src/main/java/com/example/carsmanagment/AuthenticationEntryPoint.java
@@ -0,0 +1,29 @@
+package com.example.carsmanagment;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class AuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        //super.commence(request, response, authException);
+        response.addHeader("WWW-Authenticate", "Basic realm=\\" + getRealmName() + "\\");
+        response.setContentType("application/json");
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        PrintWriter writer = response.getWriter();
+        writer.println("Accés no autoritzat");
+
+    }
+
+    @PostConstruct
+    public void initRealmname() {
+        setRealmName("videojocs.itb");
+    }
+}
